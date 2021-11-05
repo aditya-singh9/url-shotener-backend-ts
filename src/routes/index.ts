01e0c5e9be916bba110ac6788e9c58db36480b3e
Aditya Singh <75490282+aditya0930@users.noreply.github.com>
2021-11-05 18:54:12 +0530

    added validating the request

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,15 @@ import {
   createShortUrl,
   handleRedirect,
 } from "../controllers/shortUrl.controller";
+import validateResource from "../middleware/validateResource";
+import CreateShortUrlSchema from "../schema/CreateShortUrl.schema";
 
 function routes(app: Express) {
   app.get("/check", (req: Request, res: Response) => {
     return res.send("Working fine");
   });
 
-  app.post("/api/url", createShortUrl);
+  app.post("/api/url", validateResource(CreateShortUrlSchema), createShortUrl);
 
   app.get("/:shortId", handleRedirect);
 }
