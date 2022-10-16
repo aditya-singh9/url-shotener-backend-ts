93ef56495eaeae105ce1ec4ac6a4eef359865b15
Aditya Singh <aditya0930singh@gmail.com>
2022-10-16 13:22:48 +0530

    healthcheck endpoint

--- src/app.ts
+++ src/app.ts
@@ -7,26 +7,30 @@ import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import mongoSanitize from "express-mongo-sanitize";
+import { Request, Response } from "express";
 
 dotenv.config();
 const app = express();
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(express.json());
 const port = process.env.PORT || 4000;
 
 //Security Middlewares
-app.use(helmet())
+app.use(helmet());
 
-app.use(mongoSanitize()) //Against NoSQL Query Injection
+app.use(mongoSanitize()); //Against NoSQL Query Injection
 
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 100,
-  message: "Too many requests from this IP. Please try again later!"
-})
+  message: "Too many requests from this IP. Please try again later!",
+});
 
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Listening at https://localhost:${port}`);
